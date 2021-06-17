aca79d4a484c9a413aac1ce057ec5bc310540090
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-17 09:58:23 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-
-int main(){
-  puts(""Programming is like building a multilingual puzzle\n"); 
-
-return 0;
- 
+/**
+ * main
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts(""\"Programming is like building a multilingual puzzle\n");
+return(0);
 }
