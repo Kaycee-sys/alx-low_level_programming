7fc01535f301a91597d5a4f72197343d0546a42b
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-17 11:00:56 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,19 +5,15 @@
  *Return: Always 0 (success)
  */
 #include<stdio.h>
-
 int main(void)
-  
 {
-  int intType;
-  float floatType;
-  double doubleType;
-  char charType;
-  // size of evaluates the size of a variable
-  printf("Size of int: %zu bytes\n", sizeof(intType));
-  printf("Size of float: %zu bytes\n", sizeof(floatType));
-  printf("Size of double: %zu bytes\n", sizeof(doubleType));
-  printf("Size of char: %zu byte\n", sizeof(charType));
-
-  return (0);
+int intType;
+float floatType;
+double doubleType;
+char charType;
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of double: %zu bytes\n", sizeof(doubleType));
+printf("Size of char: %zu byte\n", sizeof(charType));
+return (0);
 }
