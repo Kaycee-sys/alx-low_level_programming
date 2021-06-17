7a531add09258e0d16cf52a6b60928dd9a36e5b0
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-17 11:15:51 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,14 @@ int main(void)
 {
 int intType;
 float floatType;
+long int longInt;
+long long int llInt;
 double doubleType;
 char charType;
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of long int: %zu bytes\n", sizeof(longInt));
+printf("Size of long long int: %zu bytes\n", sizeof(llInt));
 printf("Size of double: %zu bytes\n", sizeof(doubleType));
 printf("Size of char: %zu byte\n", sizeof(charType));
 return (0);
