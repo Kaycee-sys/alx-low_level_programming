31eb273145f9b0c719e6909854749d4fc76c3a1a
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-18 15:10:00 -0700

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,4 +6,20 @@
  */
 int main(void)
 {
- 
+char c = 'a';
+int num;
+
+for(num = 0; num < 16; num++)
+{
+if (num < 10)
+{
+putchar(num + '0');
+}else
+{
+putchar(c);
+c++;
+}
+}
+putchar('\n');
+return (0);
+}
