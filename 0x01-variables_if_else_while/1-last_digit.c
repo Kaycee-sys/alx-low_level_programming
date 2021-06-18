03a73a866781bc451b99d1c22ef85f586f7e9e83
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-18 07:01:45 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ if (digit > 5)
 printf("Last digit of %d is %d\" and is greater than 5 \n", n, digit);
 else if (digit == 0)
 printf("Last digit of %d is %d\" and is 0 \n", n, digit);
-else  (digit < 6)
+else if (digit < 6)
 printf("Last digit of %d is %d\" and is less than 6 and not 0 \n", n, digit);
 return (0);
 }
