e3e60e07556700e1d39a68197a650ac77c460a93
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-18 04:10:27 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*Program to print the last digit of a number*/
+int main(void)
+{
+int n;
+
+ srand(time(0));
+ n = rand() - RAND_MAX / 2;
+ printf("Enter a number: \n");
+ scanf("%d", &n);
+ digit = n%10
+   if (digit > 5)
+     printf("Last digit of %d is %d\" and is greater than 5 \n", n, digit);
+   else if (digit = 0)
+     printf("Last digit of %d is %d\" and is 0 \n", n, digit);
+   else if (digit 
+
+ return (0);
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This are solved tasks on if,else,while C programming
