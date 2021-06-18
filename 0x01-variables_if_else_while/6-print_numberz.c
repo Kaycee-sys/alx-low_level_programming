ed84cbbc88b6206aff4820c93ac54b3b5d5d15ca
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-18 12:08:29 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- *main - Entry Points
+0;276;0c *main - Entry Points
  *
  *Return: Always 0 (Success)
  */
 int main(void)
 {
 int n;
-for (n = '0' ; n <= '9' ; n++)
+for (n = 0 ; n <= 9 ; n++)
 {
-putchar( n + '0');
+putchar(n + '0');
 }
 putchar('\n');
 return (0);
