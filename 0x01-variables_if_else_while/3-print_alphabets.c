519a63402d6d7e3fb5c958d96e6330b98b6b3d33
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-18 08:32:38 -0700

    Lower and upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 /**
  *main - A program that prints alphabets in lower case and upper case
+ *
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-  
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+putchar (ch);
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+putchar (ch);
+return 0;
+}
