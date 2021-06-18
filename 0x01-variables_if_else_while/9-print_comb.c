841b3a738ae95b192dde57ef64812c5dfea481f7
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-18 13:20:05 -0700

    combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-* main -Entry point
+0;276;0c* main -Entry point
 *
 *Return: Always 0 (Success)
 */
 int main(void)
 {
 int a;
-for (a = '0'; a<= '9'; a++)
+for (a = '0'; a <= '9'; a++)
 {
 putchar(a);
 if (a != '9')
