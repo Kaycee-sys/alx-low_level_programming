523f09348a4447adfa091a91dda2faaec263c942
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-18 12:25:33 -0700

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,6 +6,12 @@
  */
 int main(void)
 {
-  char ch;
-
-  
+char ch; letter
+for (ch = 'a' ; ch <= 'z' ; ch++)
+if (letter ! = 'e' && letter ! = 'q')
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
