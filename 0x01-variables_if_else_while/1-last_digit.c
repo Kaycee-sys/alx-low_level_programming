21f8eab8f3dd25b61a99de727ee8671b6f128099
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-18 07:43:09 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ n = rand() - RAND_MAX / 2;
 scanf("%d", &n);
 digit = n % 10;
 if (digit > 5)
-printf("Last digit of %d is %d and is greater than 5 \n", n, digit);
+printf("Last digit of %d is %d\" and is greater than 5", n, digit);
 else if (digit == 0)
-printf("Last digit of %d is %d and is 0 \n", n, digit);
+printf("Last digit of %d is %\d and is 0", n, digit);
 else if (digit < 6)
-printf("Last digit of %d is %d and is less than 6 and not 0 \n", n, digit);
+printf("Last digit of %d is %\d and is less than 6 and not 0", n, digit);
 return (0);
 }
