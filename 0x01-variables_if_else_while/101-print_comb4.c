c72db09c8a54008b90da199b9e44a2f40b9232ef
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-18 16:31:14 -0700

    different combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main - program that prints all possible different combinations of 
+ *main - program that prints all possible different combinations of
  *three digits
  *
  *Return: Always 0 (Success)
@@ -14,7 +14,7 @@ for (i = 48; i < 58; i++)
 {
 for (n = 48; n < 58; n++)
 {
-if (n > 1)
+if (n > i)
 {
 for (j = 48; j < 58; j++)
 {
