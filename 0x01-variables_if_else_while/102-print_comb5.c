a957f5fc2a289b17479f5c258dabcc430376687f
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-18 17:05:48 -0700

    different combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,14 +12,14 @@ for (i = 0; i < 100; i++)
 {
 for (j = 0; j < 100; j++)
 {
-if (j > 1)
+if (j > i)
 {
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
-if (i * 100 + j!= 9899)
+if (i * 100 + j! = 9899)
 {
 putchar(',');
 putchar(' ');
