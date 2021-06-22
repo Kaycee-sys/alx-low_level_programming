38b3baeda645ae32a559eef0b2677d27a279ce1c
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-22 07:26:47 -0700

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,12 +6,12 @@
 */
 int main(void)
 {
-  char holberton[9] = "Holberton";
-    int i;
-  for (i = 0; i < 9; i++)
-    {
-      _putchar(holberton[i]);
-    }
-  _putchar('\n');
-  return (0);
+char holberton[9] = "Holberton";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(holberton[i]);
+}
+ _putchar('\n');
+return (0);
 }
