369c0bffeb9f9415840a5ac716b24a340d1a1674
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-22 08:00:55 -0700

    10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * 2-print_alphabet_x10.c - 'Program that prints alphabets 10 times'
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+char c = 'a';
+int i = 0;
+for (i = o; i < 10, i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n')
+}
+}
