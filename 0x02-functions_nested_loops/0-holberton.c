65eb1a2f7e6d9b21d4c3f485a4655ed064ad17c6
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-22 05:02:40 -0700

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "holberton.h"
 /**
 *main - Program that prints Holberton
 *
@@ -6,7 +7,6 @@
 */
 int main(void)
 {
-  char ch = "Holberton\n"
-    putchar(ch);
+  putchar(ch);
   return(0);
 }
