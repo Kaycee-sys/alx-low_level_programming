ba7f2a9e0854e4319310ec1557f9c2e760149447
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-22 08:24:43 -0700

    upper or lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _islower - 'Program that checks if a character is upper or lower case'
+ * Return: void
+ */
+int _islower(int c)
+{
+if (islower(c))
+return (1);
+else
+return (0);
+}
