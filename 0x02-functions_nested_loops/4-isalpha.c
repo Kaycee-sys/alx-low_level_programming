697dc9d2224e8ce985690871197a330e35c861ce
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-22 09:23:24 -0700

    alphabetic

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Program that checks if the character is alpahbetic
+ * @c:  is an integer
+ * Return: 1 if alphabetic else 0
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
