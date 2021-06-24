cd2353d29814648d1ea6edae31fc3004c4d16773
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-24 06:17:21 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Program that prints that numbers 0-9
+ *
+ * Return: void
+ */
+
+void print_numbers(void);
+{
+char ch;
+for (c = "0"; c <= "9"; c++)
+{
+_putchar(ch);
+}
+_putchar("\n");
+}
