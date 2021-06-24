8313b588fc1982d52ed8de6ad0b1870812a79ea4
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-24 09:43:43 -0700

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,13 @@
  * Return: void
  */
 
-
 void print_diagonal(int n)
 {
 int i, j;
 
 if (n <= 0)
 _putchar('\n');
- for (i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
 for (j = 0; j < i; j++)
 {
@@ -22,4 +21,5 @@ _putchar(' ');
 }
 _putchar('\\');
 _putchar('\n');
+}
 }
