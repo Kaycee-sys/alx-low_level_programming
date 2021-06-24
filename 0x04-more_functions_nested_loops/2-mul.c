24dbd24b7c9aafa7df996d8f7189a819b517f29b
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-24 05:08:54 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *_mul(int a, int b) - Program that multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: multiplication of a and b
+ */
+
+
+int _mul(int a, int b)
+{
+return (a*b);
+}
