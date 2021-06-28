908135226569dc41b6c996f71f2626c2d30696f2
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-28 07:48:44 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * prin_rev - Prints a string in reverse
+ * @str: String to be printed
+ *
+ * Return: void
+ */
+void print_rev(char *s);
+{
+int i = 0;
+while (str[i]) i++;
+i = i - 1;
+{
+_putchar(str[i]);
+i--;
+}
+_putchar('\n');
+}
