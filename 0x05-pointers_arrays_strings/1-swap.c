3697b9d93206428eba02133a9d28089b48a506d5
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-06-28 05:18:01 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Swaps the values of int a and int b
+ * @a - first int
+ * @b - second int
+ *
+ * Return : void
+ */
+void swap_int(int *a, int *b);
+{
+int c;
+c = *a;
+*a = *b;
+c = *b;
+}
