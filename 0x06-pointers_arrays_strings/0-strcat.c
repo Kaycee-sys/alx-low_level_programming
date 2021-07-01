ee3a6318ceb309e71bb4a826de644354a24d7561
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-07-01 03:01:42 -0700

    cocatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,7 +12,6 @@
 char *_strcat(char *dest, char *src)
 {
 int i = 0, j = 0;
-
 while (dest[i] != '\0')
 {
 i++;
@@ -23,7 +22,6 @@ dest[i] = src[j];
 j++;
 i++;
 }
- 
 dest[i] = '\0';
 return (dest);
 }
