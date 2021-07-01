227a16daf4da7fee8233f762940e4ff1f64d962a
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-07-01 04:03:54 -0700

    cocatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,16 +2,15 @@
 #include "holberton.h"
 
 /**
- * *_strcat -  cocatenates two strings.
- * @dest : string to be appended 
- * @src : string to append
- * Return : Always 0
+ * *_strcat - concatenates two strings.
+ * @dest : first string.
+ * @src : second string.
+ * Return: Always 0.
  */
 
 char *_strcat(char *dest, char *src)
 {
 int i = 0, j = 0;
- 
 while (dest[i] != '\0')
 {
 i++;
@@ -22,8 +21,6 @@ dest[i] = src[j];
 j++;
 i++;
 }
- 
 dest[i] = '\0';
 return (dest);
 }
-
