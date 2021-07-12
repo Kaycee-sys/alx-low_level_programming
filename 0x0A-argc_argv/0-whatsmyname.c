999cf4ec8592073908844b82756e221587f925e4
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-07-12 03:52:01 -0700

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints the name of this program
+ * main - Print the name of the executable file
+ * @argc: Argument count
+ * @argv: Array of argument strings
  *
- * Return: 0 (always successfull)
+ * Return: 0 for succesful exit
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
