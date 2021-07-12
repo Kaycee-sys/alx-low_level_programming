c39c9fb2380479f36c7ae43c60aa90869ab96a7b
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-07-12 07:09:59 -0700

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,6 +2,8 @@
 
 /**
  * main - Prints the product of two numbers passed to this program
+ * @argc: Number of arguments
+ * @argv: Array of arguments recieved 
  *
  * Return: 0 if successful, otherwise 1
  */
@@ -9,16 +11,16 @@ int main(int argc, char *argv[])
 {
 int num1, num2;
 
-if (argc == 3)
-{
-sscanf(argv[1], "%d", &num1);
-sscanf(argv[2], "%d", &num2);
-printf("%d\n", num1 * num2);
-return (0);
-}
-else
-{
-puts("Error");
-return (1);
-}
+	if (argc == 3)
+	{
+		sscanf(argv[1], "%d", &num1);
+		sscanf(argv[2], "%d", &num2);
+		printf("%d\n", num1 * num2);
+		return (0);
+	}
+	else
+	{
+		puts("Error");
+		return (1);
+	}
 }
