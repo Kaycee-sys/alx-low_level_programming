5699262d36569cd2ca59b3c79b2015eb1043104e
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-07-12 07:14:50 -0700

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,16 @@
 int main(int argc, char *argv[])
 {
 int num1, num2;
-
-	if (argc == 3)
-	{
-		sscanf(argv[1], "%d", &num1);
-		sscanf(argv[2], "%d", &num2);
-		printf("%d\n", num1 * num2);
-		return (0);
-	}
-	else
-	{
-		puts("Error");
-		return (1);
-	}
+if (argc == 3)
+{
+sscanf(argv[1], "%d", &num1);
+sscanf(argv[2], "%d", &num2);
+printf("%d\n", num1 * num2);
+return (0);
+}
+else
+{
+puts("Error");
+return (1);
+}
 }
