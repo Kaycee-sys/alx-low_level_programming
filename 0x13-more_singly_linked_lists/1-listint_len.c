459e78da4a9e9ac3042eae8615ff6af882bef5a5
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-08-03 10:44:35 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,12 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked listint_t list.
- * @h: pointer to the linked list
- *
- * Return: number of elements in a linked listint_t list
+ * listint_len - gets number of elements in a linked listint_t list.
+ * @h: pointer to listint_t
+ * Return: the number of nodes
  */
+
 size_t listint_len(const listint_t *h)
 {
 	size_t nodes_count = 0;
