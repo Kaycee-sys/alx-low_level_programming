2306e3e9789a7592c5e9dd8b343f8cd6551c0186
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-08-13 09:46:25 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text at the end of a file
+ * create_file - Creates a file with a given content
  * @filename: The name of the file to create
- * @text_content: The content to be added at the file
+ * @text_content: The content of the file
  *
  * Return: 1 if successful, otherwise -1
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, i;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_APPEND, 0600);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd >= 0)
 	{
 		for (i = 0; text_content != NULL && *(text_content + i) != '\0'; i++)
@@ -21,7 +21,8 @@ int append_text_to_file(const char *filename, char *text_content)
 			if (write(fd, text_content + i, 1) != 1)
 				return (-1);
 		}
-		close(fd);
+		if (close(fd) == -1)
+			return (-1);
 		return (1);
 	}
 	return (-1);
