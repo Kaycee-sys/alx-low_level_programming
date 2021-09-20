7ca6cdaf0ead0fc42efc016e98bd17a63880e0b0
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-09-20 15:07:35 -0700

    task 0

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-gcc *.c -c -fPIC
-gcc *.o -shared -o liball.so
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -fpic -c *.c
+gcc -shared -o liball.so *.o
+chmod 644 liball.so
+export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
